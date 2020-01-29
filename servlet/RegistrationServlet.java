190d9a0c3a21f24554a82e30ad4beb8e86c6d6fe
tortilla79 <bobu@bobu.bobu>
2020-01-30 00:36:04 +0100

    start RegistrationServlet

---
+++ servlet/RegistrationServlet.java
@@ -0,0 +1,90 @@
+
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/Registration")
+public class RegistrationServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response ) {
+	PrintWriter out = response.getWriter();
+        StringBuilder sb = new StringBuilder(); 
+	BufferedReader br = request.getReader(); //per leggere dati da POST
+	String tmp = null;
+
+	while ((tmp = br.readLine()) != null){
+	    sb.append(tmp);
+	}
+
+	tmp = sb.toString();
+
+	
+	//deserializzo i dati
+	Gson gson = new Gson();
+	LoginData logdata = gson.fromGson(tmp, LoginData.class);
+
+	// verifico che la pssword sia stata confermata correttamente
+	boolean match = (logdata.password == logdata.passwordrip);
+
+	if (!match) {
+	    out.print("psw");
+	    out.close();
+	} else { 	// verifico l'eventuale presenza di uname nel database
+	    try (
+		 // Step 1: Allocate a database 'Connection' object
+		 Connection conn = DriverManager.getConnection(
+							       "jdbc:mysql://localhost:3306/ebookshop?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+							       "myuser");   // For MySQL
+		 // The format is: "jdbc:mysql://hostname:port/databaseName", "username", "password"
+
+		 // Step 2: Allocate a 'Statement' object in the Connection
+		 Statement stmt = conn.createStatement();
+		 );
+	    String sqlStr = "SELECT * FROM user WHERE username = '" + logdata.username + "'";
+	    ResultSet rset = stmt.executeQuery(sqlStr);
+
+	    if (rset.next()) { // Se l'username gia esiste lo comunico al client
+		out.print("usr");
+	    } else {// registro il nuovo utente
+		sqlStr = "INSERT INTO user (privilege, username, password, email) VALUES('client','" +
+		    logdata.username +"','"+ logdata.password + "','" + logdata.email+ "')"; 
+		stmt.executeQuery(sqlStr);
+		out.print("ok");
+	    }
+
+	}
+
+
+	
+	
+    }
+    
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException
+    {
+	processRequest(request, response);
+    }
+
+    /**
+     * We are going to perform the same operations for POST requests
+     * as for GET methods, so this method just sends the request to
+     * the doGet method.
+     */
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException
+    {
+    	processRequest(request, response);
+    }
+}
+
+class LoginData{
+    String username;
+    String email;
+    String password;
+    String passwordrip;
+
+     
+}
