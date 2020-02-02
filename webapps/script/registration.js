5636915dc96563c138d29240ccb3ca7803759ce5
tortilla79 <bobu@bobu.bobu>
2020-02-02 20:33:31 +0100

    database con registrazione - 1

--- webapps/script/registration.js
+++ webapps/script/registration.js
@@ -18,15 +18,21 @@ angular.module('registration', [])
 		    case "uname":
 			$scope.requestans = "username gia in uso";
 			break;
-		    case "psw":
+		    case "pswrip":
 			$scope.requestans = "conferma password errata";
+			break;
+		    case "psw":
+			$scope.requestans = "password troppo corta - minimo 8 caratteri";
 			break;
 		    default:
 			$scope.requestans = msg;
 		    }
-		    if ($scope.requestans === "username gia in uso")
+		    if ($scope.requestans === "success")
+			document.location.href = 'home.html';
+		    else if ($scope.requestans === "username gia in uso")
 			document.getElementById("username").focus();
-		    else if($scope.requestans === "conferma password errata") {
+		    else if($scope.requestans === "conferma password errata" ||
+			   $scope.requestans === "password troppo corta - minimo 8 caratteri") {
 			$scope.password = "";
 			$scope.passwordrip = "";
 			document.getElementById("password").focus();
