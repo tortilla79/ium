4cf0481435c02e52a4d4e50880585a069d56f4c6
tortilla79 <bobu@bobu.bobu>
2020-02-02 19:30:29 +0100

    registrazione con database

--- webapps/WEB-INF/classes/src/RegistrationServlet.java
+++ webapps/WEB-INF/classes/src/RegistrationServlet.java
@@ -29,71 +29,75 @@ public class RegistrationServlet extends HttpServlet {
 	logdata.email = nameValue.get("email");
 	logdata.password = nameValue.get("password");
 	logdata.passwordrip = nameValue.get("passwordrip");
-
-	// verifico che la pssword sia stata confermata correttamente
 	boolean match = logdata.password.equals(logdata.passwordrip);
-	
-	if (!match) {
+	if (logdata.password.length() < 8) { // verifico che la password sia di almeno 8 caratteri
 	    out.print("psw");
+	    out.close();   
+	}
+	else if (!match) { 	// verifico che la pssword sia stata confermata correttamente
+	    out.print("pswrip");
 	    out.close();
-	}/* else { 	// verifico l'eventuale presenza di uname nel database
+	} else { 	// verifico l'eventuale presenza di uname nel database
 	    try (
-	    // Step 1: Allocate a database 'Connection' object
-	    Connection conn = DriverManager.getConnection(
-	    "jdbc:mysql://localhost:3306/ebookshop?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
-	    "myuser");   // For MySQL
-	    // The format is: "jdbc:mysql://hostname:port/databaseName", "username", "password"
+		 // Step 1: Allocate a database 'Connection' object
+		 Connection conn = DriverManager.getConnection(
+							       "jdbc:mysql://localhost:3306/sblocknotes?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+							       "bobu", "gattopardo");   // For MySQL
+		 // The format is: "jdbc:mysql://hostname:port/databaseName", "username", "password"
 
-	    // Step 2: Allocate a 'Statement' object in the Connection
-	    Statement stmt = conn.createStatement();
-	    );
-	    String sqlStr = "SELECT * FROM user WHERE username = '" + logdata.username + "'";
-	    ResultSet rset = stmt.executeQuery(sqlStr);
+		 // Step 2: Allocate a 'Statement' object in the Connection
+		 Statement stmt = conn.createStatement();
+		 ) {
+		String sqlStr = "SELECT * FROM user WHERE username = '" + logdata.username + "'";
+		ResultSet rset = stmt.executeQuery(sqlStr);
 	    
-	    if (rset.next()) { // Se l'username gia esiste lo comunico al client
-	    out.print("usr");
-	    } else {// registro il nuovo utente
-	    sqlStr = "INSERT INTO user (privilege, username, password, email) VALUES('client','" +
-	    logdata.username +"','"+ logdata.password + "','" + logdata.email+ "')"; 
-	    stmt.executeQuery(sqlStr);
-	    out.print("ok");
+		if (rset.next()) { // Se l'username gia esiste lo comunico al client
+		    out.print("uname");
+		} else {// registro il nuovo utente
+		    sqlStr = "INSERT INTO user (privilege, username, password, email) VALUES('client','" +
+			logdata.username +"','"+ logdata.password + "','" + logdata.email+ "')"; 
+		    stmt.executeQuery(sqlStr);
+		    out.print("ok");
+		}
+	    } catch (Exception ex) {
+		out.print("dberror");
+		ex.printStackTrace();
 	    }
-	  */
-
-	String registered = "piero";
-	if (logdata.username.equals(registered))
-	    out.print("uname");
-	else
-	    out.print(logdata.toString());
-	out.close();
-	    
+	}
+	/*    String registered = "piero";
+	    if (logdata.username.equals(registered))
+		out.print("uname");
+	    else
+		out.print(logdata.toString());
+	    out.close();
+	*/  
     }
 	
 	
     
     
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-	throws IOException, ServletException
-    {
-	processRequest(request, response);
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	    throws IOException, ServletException
+	    {
+		processRequest(request, response);
+	    }
+
+	/**
+	 * We are going to perform the same operations for POST requests
+	 * as for GET methods, so this method just sends the request to
+	 * the doGet method.
+	 */
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+	    throws IOException, ServletException
+	    {
+		processRequest(request, response);
+	    }
     }
 
-    /**
-     * We are going to perform the same operations for POST requests
-     * as for GET methods, so this method just sends the request to
-     * the doGet method.
-     */
-
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-	throws IOException, ServletException
-    {
-	processRequest(request, response);
+    class RegistrationData{
+	String username;
+	String email;
+	String password;
+	String passwordrip;
     }
-}
-
-class RegistrationData{
-    String username;
-    String email;
-    String password;
-    String passwordrip;
-}
