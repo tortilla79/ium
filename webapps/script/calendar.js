e1af68451007d8a4bcd2559e1904f59acd7b62b8
tortilla79 <bobu@bobu.bobu>
2020-02-04 09:26:53 +0100

    getCalendar xhrequest

--- webapps/script/calendar.js
+++ webapps/script/calendar.js
@@ -1,11 +1,16 @@
-angular.module('calendar', [])
-    .controller('calendarCtrl', function ($scope, $http) {
-
-	$http.get("Calendar")
-	    .then(function (response) {
-		let msg = response.data;
-		tabledata = document.querySelectorAll('td');
-		for (let i=0; i<tabledata.length; i++)
-		    tabledata[i] = msg;
-	    });
-    });
+document.addEventListener("DOMContentLoaded", function(){
+    
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+	if (xhr.readyState == 4 && xhr.status == 200) {
+	    let tdatas = document.querySelectorAll('td');
+	    for(let i=0; i<tdatas.length; i++)
+		tdatas[i].innerHTML=xhr.responseText;
+	}
+    }
+    xhr.open('GET', "Calendar", true);
+    xhr.send(null);
+    
+    //popTable();
+    
+});
