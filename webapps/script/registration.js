7ce4ab8111607337bc7856cda3dbf735a5e0009a
tortilla79 <bobu@bobu.bobu>
2020-02-27 13:55:09 +0100

    registrazione completata

--- webapps/script/registration.js
+++ webapps/script/registration.js
@@ -19,14 +19,22 @@ angular.module('registration', [])
 			$scope.requestans = "username gia in uso";
 			break;
 		    case "psw":
+			$scope.requestans = "password troppo corta - min 8 caratteri";
+			break;
+		    case "pswrip":
 			$scope.requestans = "conferma password errata";
 			break;
 		    default:
 			$scope.requestans = msg;
 		    }
-		    if ($scope.requestans === "username gia in uso")
+		    if ($scope.requestans == "success") {
+			if(!alert("Registrazione Completata con Successo"))
+			    window.location.replace("home.html");
+		    }
+		    else if ($scope.requestans === "username gia in uso")
 			document.getElementById("username").focus();
-		    else if($scope.requestans === "conferma password errata") {
+		    else if($scope.requestans === "conferma password errata" ||
+			    $scope.requestans === "password troppo corta - min 8 caratteri") {
 			$scope.password = "";
 			$scope.passwordrip = "";
 			document.getElementById("password").focus();
